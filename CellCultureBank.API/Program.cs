545db9f891d2c460a50d18364d35281311d2ddc6
Maxim Vaskin <maxifly0202@gmail.com>
2024-09-06 12:17:56 +0300

    point commit

--- CellCultureBank.API/Program.cs
+++ CellCultureBank.API/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddSwaggerGen(options =>
 {
     options.SwaggerDoc("v1", new OpenApiInfo
     {
-        Version = "v1.0.1",
+        Version = "v1.0.2",
         Title = "Банк клеточных культур",
         Description = "Система, посредством которой производят последовательные серии продукции с использованием клеточных культур, принадлежащих одному и тому же главному банку клеток.",
 

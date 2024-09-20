97f0694fbc5eb8fec1917d41c663ce7febd037a7
Maxim Vaskin <105639741+Haderdsins@users.noreply.github.com>
2024-09-20 10:47:21 +0300

    Delete

--- CellCultureBank.WEB/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace CellCultureBank.WEB.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
